3172b0ae934b8a622431e29c4d74ff829c28e64c
iAHTOH <k.ahtoh@gmail.com>
2023-06-19 09:23:16 +0300

    Update settings.py
    Что то картинки не отдаются.

--- configs/settings.py
+++ configs/settings.py
@@ -160,7 +160,11 @@ CACHE_MIDDLEWARE_KEY_PREFIX = "sopds"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+#STATIC_ROOT = 'static'
+# Что то картинки не отдаются. Вот это работает
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 CONSTANCE_BACKEND = 'constance.backends.database.DatabaseBackend'
 
